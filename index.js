2b96c000b33d47a1b99698c9358709f6c88c2c49
Jithu Joshy Jy <64597606+jithujoshyjy@users.noreply.github.com>
2022-08-18 02:05:50 +0530

    fix data attribute issue in custom elements

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import * as jquery from "./node_modules/jquery/dist/jquery.min.js"
 
 /**
- * @typedef {{ name: string, create: () => { type: "custom", name: string, body: DocumentFragment, data: object } }} CustomElement
+ * @typedef {{ type: "html", name: string, body: DocumentFragment, data: object, parent: JQElement }} JQHTMLElement
+ */
+/**
+ * @typedef {{ type: "custom", name: string, body: DocumentFragment, data: object, parent: JQElement }} JQCustomElement
+ */
+/**
+ * @typedef {{ type: "partial", name: string, parent: null, create: (children: ChildNode[], data: {}) => JQCustomElement }} JQPartialElement
  */
 
 /**
@@ -9,82 +15,98 @@ import * as jquery from "./node_modules/jquery/dist/jquery.min.js"
  */
 
 /**
- * @typedef {{ type: "html" | "custom" | "custom-constructor" | "partial", body: HTMLElement | CustomElement | DocumentFragment, data: object, name?: string, parent: JqElement | null}} JqElement
+ * @typedef {JQHTMLElement | JQCustomElement | JQPartialElement} JQElement
  */
 
 /**
- * @type {Array<{ name: string, create: () => ProxyConstructor>}}
+ * @type {JQPartialElement[]}}
  */
 const newElements = new Array()
 
 /**
- * @type {Array<JqElement>}
+ * @type {JQElement[]}
  */
 const elementStack = new Array()
 
 /**
- * @type {JqElement?}
+ * @type {JQElement?}
  */
 let currentElement = null
+/**
+ * @type {JQElement | Text | null}
+ */
+let currentNode = null
 
 const jquire = {
     /**
-     * @param {Object<string, { type: "partial", body: DocumentFragment, data: object }>} newElmsObj An object containing custom elements
+     * @param {Object<string, JQPartialElement>} partialElmConstructors An object containing custom elements
      * @returns {void}
      * @description Adds all the elements from first argument to newElements array.
      */
-    add(newElmsObj) {
-        for (let key of Object.keys(newElmsObj)) {
+    add(partialElmConstructors) {
+        for (let partialElmName of Object.keys(partialElmConstructors)) {
 
-            const _newElmObj = Object.create(null)
-            _newElmObj.name = key
-            _newElmObj.type = "custom-constructor"
-            _newElmObj.parent = null
-            _newElmObj.create = function () {
-                const fragment = newElmsObj[key].body
-                const defaultData = newElmsObj[key].data
+            /**
+             * @type {JQPartialElement}
+             */
+            const newPartialElement = Object.create(null)
+
+            newPartialElement.name = partialElmName
+            newPartialElement.type = "partial"
+            newPartialElement.parent = null
+
+            newPartialElement.create = function (children = [], data = {}) {
+                const fragment = document.createDocumentFragment()
                 const parentElm = elementStack.slice(-1)?.[0]
+
+                /**
+                 * @type {JQCustomElement}
+                 */
                 const elm = {
-                    type: "custom", name: key, body: fragment, data: { ...defaultData }, parent: parentElm ?? null
+                    type: "custom", name: partialElmName, body: fragment, data, parent: parentElm ?? null
                 }
-                currentElement = elm
+
+                /**
+                 * @type {() => JQPartialElement}
+                 */
+                const newElmConstructor = arrowFnToRegularFn(partialElmConstructors[partialElmName]).bind(elm)
+
+                if (typeof newElmConstructor != "function")
+                    throw new TypeError(`InvalidComponentConstructor: Expected a function instead got ${typeof newElmConstructor}`)
+
+                elementStack.push(elm)
+                currentElement = currentNode = elm
+
+                newElmConstructor()
+                elementStack.pop()
+
+                for (let child of children)
+                    elm.body.append(child)
 
                 return elm
             }
-            newElements.push(_newElmObj)
+            newElements.push(newPartialElement)
         }
     },
+
     /**
-     * @returns {ProxyConstructor} A proxy that'll intercept all property access
-     * @description Creates a new custom element
+     * @returns {ProxyConstructor new<(strings?: string | null, ...values: string[]) => Text>}
      */
-    new() {
-        const fragment = document.createDocumentFragment()
-        const elm = { type: "partial", body: fragment, data: {}, parent: null }
-
-        elementStack.push(elm)
-        const _currentElement = currentElement
-        currentElement = elm
-
-        return new Proxy(elm, {
-            get(target, prop, reciever) {
-                if (prop in target)
-                    return target[prop]
-
-                currentElement = _currentElement ?? null
-                return elementStack.pop()
-            }
-        })
-    },
-    text(strings = null, ...values) {
+    text() {
         const parentElm = elementStack.slice(-1)?.[0]
 
+        /**
+         * @param {string?} strings 
+         * @param  {...string} values 
+         * @returns {Text}
+         */
         const createTextNodeFromArgs = (strings = null, ...values) => {
             const result = [strings[0]]
-            values.forEach((value, i) => 
+            values.forEach((value, i) =>
                 result.push(value, strings[i + 1]))
-            
+
             const textNode = document.createTextNode(result.join(''))
+            currentNode = textNode
             parentElm && parentElm.body.append(textNode)
             return textNode
         }
@@ -93,15 +115,24 @@ const jquire = {
             get(target, prop, reciever) {
                 const textNode = document.createTextNode(prop)
                 parentElm && parentElm.body.append(textNode)
+                currentNode = textNode
                 return textNode
             }
         })
     },
+    /**
+     * 
+     * @returns {ProxyConstructor new<JQCustomElement>(target: JQCustomElement, handler: ProxyHandler<JQCustomElement>) => JQCustomElement}
+     */
     render() {
         const fragment = document.createDocumentFragment()
+        /**
+         * @type {JQCustomElement}
+         */
         const elm = { type: "custom", name: "render", body: fragment, data: {}, parent: null }
+
         const _currentElement = currentElement
-        currentElement = elm
+        currentElement = currentNode = elm
         elementStack.push(elm)
 
         return new Proxy(elm, {
@@ -109,7 +140,7 @@ const jquire = {
                 if (prop in target)
                     return target[prop]
 
-                currentElement = _currentElement ?? null
+                currentElement = currentNode = _currentElement ?? null
 
                 if (!("tag" in target.data))
                     throw new Error(`NoRenderTarget: The required 'tag' attribute is missing.`)
@@ -159,7 +190,7 @@ const _parents = new Proxy(function (ctx = this) {
     return new Proxy(getCurrentElm(), {
         get(target, prop, reciever) {
             /**
-             * @param {JqElement} elm 
+             * @param {JQElement} elm 
              * @param {string} prop 
              * @returns 
              */
@@ -171,7 +202,7 @@ const _parents = new Proxy(function (ctx = this) {
 }, {
     get(target, prop, reciever) {
         /**
-         * @param {JqElement} elm 
+         * @param {JQElement} elm 
          * @param {string} prop 
          * @returns 
          */
@@ -195,15 +226,19 @@ const _ = new Proxy({}, {
     get: handleAttrProxyPropAccess
 })
 
+_.prototype[Symbol.toPrimitive] = function (hint) {
+    return ""
+}
+
 /**
  * @param {string} elmName 
- * @returns {JqElement}
+ * @returns {ProxyConstructor new<JQElement>}
  * @throws Will throw an error if the first argument is not a valid HTML element or a custom element
  * @description Creates an HTML element based on the case of the element name; if the element name is in lower case, create an HTML element else create a custom element.
  */
 function createElement(elmName) {
     if (/^[a-z]/.test(elmName))
-        return createHTMLElement(elmName)
+        return createJQHTMLElement(elmName)
     else if (/^[A-Z]/.test(elmName))
         return createCustomElement(elmName)
     else
@@ -212,37 +247,45 @@ function createElement(elmName) {
 
 /**
  * @param {string} elmName 
- * @returns {ProxyConstructor}
+ * @returns {ProxyConstructor new<JQHTMLElement>}
  * @throws Will thow an error if the first argument is not a valid HTML element.
  */
-function createHTMLElement(elmName) {
-    const elm = { type: "html", body: document.createElement(elmName), data: {} }
+function createJQHTMLElement(elmName) {
+    const parentElm = elementStack.slice(-1)?.[0]
+    /**
+     * @type {JQHTMLElement}
+     */
+    const elm = { type: "html", name: elmName, body: document.createElement(elmName), data: {}, parent: parentElm ?? null}
+
     if (elm.body instanceof HTMLUnknownElement)
         throw new Error(`InvalidHTMLElement: '${elmName}' is not a valid HTML element.`)
-
-    const parentElm = elementStack.slice(-1)?.[0]
-    elm.parent = parentElm ?? null
+    
     elementStack.push(elm)
-    currentElement = elm
+    currentElement = currentNode = elm
 
     return new Proxy(elm, {
         get(target, prop, reciever) {
             if (prop in target)
                 return target[prop]
 
-            parentElm && parentElm.body.append(elm.body)
-            currentElement = parentElm ?? null
+            const hasOnlyStringBody = elm.body.childNodes.length == 0 &&
+                Object.keys(elm.data).length == 0 &&
+                elm.body.attributes.length == 0
 
-            if (elementStack.length > 1)
-                return elementStack.pop()
-            return target
+            if (hasOnlyStringBody)
+                elm.body.append(prop)
+
+            parentElm && parentElm.body.append(elm.body)
+            currentElement = currentNode = parentElm ?? null
+
+            return elementStack.pop()
         }
     })
 }
 
 /**
  * @param {string} elmName
- * @returns {{ type: "custom", body: ProxyConstructor }}
+ * @returns {ProxyConstructor new<JQCustomElement>}
  * @throws Will throw an error if the custom element is not defined.
  */
 function createCustomElement(elmName) {
@@ -251,32 +294,36 @@ function createCustomElement(elmName) {
     if (!newElementConstructor)
         throw new Error(`NotDefined: Custom element - '${elmName}' is not defined.`)
 
-    const newElement = newElementConstructor.create()
     const parentElm = elementStack.slice(-1)?.[0]
-    newElement.parent = parentElm ?? null
+    /**
+     * @type {JQCustomElement}
+     */
+    let newElement = {
+        type: "custom", name: elmName, body: new DocumentFragment(), data: {}, parent: parentElm ?? null
+    }
 
     elementStack.push(newElement)
-    currentElement = newElement
+    currentElement = currentNode = newElement
 
     return new Proxy(newElement, {
         get(target, prop, reciever) {
             if (prop in target)
                 return target[prop]
 
+            const childHTMLElements = Array.from(newElement.body.childNodes)
+            newElement = newElementConstructor.create(childHTMLElements, newElement.data)
+
             parentElm && parentElm.body.append(newElement.body)
-            currentElement = parentElm ?? null
+            currentElement = currentNode = parentElm ?? null
 
-            if (elementStack.length > 1)
-                return elementStack.pop()
-
-            return target
+            return elementStack.pop()
         }
     })
 }
 
 /**
  * @param {string} attr
- * @param {JqElement} elm
+ * @param {JQElement} elm
  * @param {any} defVal
  * @returns {void}
  * @throws Will throw an error if the attribute name contains invalid characters.
@@ -293,9 +340,9 @@ function createAttribute(attr, elm, defVal = null) {
     }
 }
 
-/**
- * @param {Attr} attr
- * @param {HTMLElement} elm
+/** 
+ * @param {{attrName: string, attrObj: Attr}}
+ * @param {JQHTMLElement} elm
  * @param {any} defVal
  * @returns {void}
  * @throws Will throw an error if the attribute is not a valid html attribute.
@@ -312,7 +359,7 @@ function createHTMLAttribute({ attrName, attrObj }, elm, defVal = "") {
 
 /**
  * @param {string} attr
- * @param {JqElement} elm
+ * @param {JQElement} elm
  * @param {any} defVal
  * @returns {void}
  * @throws Will throw an error if the attribute is not a valid html attribute.
@@ -358,7 +405,11 @@ function arrowFnToRegularFn(callback) {
     if (isArrowFn) {
         const isParamsInParantheses = stringifiedCallback.trim().startsWith("(")
         const [fnParams, ...fnBody] = stringifiedCallback.split("=>")
-        _callback = eval("(function" + (!isParamsInParantheses ? "(" : "") + fnParams + (!isParamsInParantheses ? ")" : "") + "{" + fnBody.join("=>") + "})")
+        const hasBlockFnBody = fnBody[0].trim().startsWith("{")
+        _callback = eval("(function" + (!isParamsInParantheses ? "(" : "") +
+            fnParams + (!isParamsInParantheses ? ")" : "") +
+            (!hasBlockFnBody ? "{ return" : "") + fnBody.join("=>") +
+            (!hasBlockFnBody ? "}" : "") + ")")
     }
     return _callback
 }
@@ -367,7 +418,7 @@ function arrowFnToRegularFn(callback) {
  * @param {QueryFunction} target 
  * @param {string} prop 
  * @param {QueryFunction} reciever 
- * @returns {HTMLElement | function | ProxyConstructor}
+ * @returns {JQHTMLElement | function | ProxyConstructor}
  */
 
 function handleElmProxyPropAccess(target, prop, reciever) {
@@ -384,14 +435,16 @@ function handleElmProxyPropAccess(target, prop, reciever) {
 }
 
 /**
- * @param {Object} target 
+ * @param {object} target 
  * @param {string} prop
- * @param {Object} reciever
+ * @param {object} reciever
  * @returns {void}
  * @throws Throws an error when accessing an attribute ourtside of an element.
  */
 
 function handleAttrProxyPropAccess(target, prop, reciever) {
+    if (prop == "prototype")
+        return Object.getPrototypeOf(target)
     if (!currentElement)
         throw new Error(`OutOfScopeAttribute: Attribute '${prop}' cannot be accessed outside an element`)
     return currentElement.data[prop]
