36422be4661e0e5bdc4ecc250f7d194419f4a49a
jithujoshyjy <jithujoshyjy@gmail.com>
2023-07-09 00:06:23 +0530

    fix tsconfig getting ignored

---
+++ types/index.d.ts
@@ -0,0 +1,2 @@
+/// <reference path="./utility.d.ts" />
+/// <reference path="./jquire.d.ts" />
---
+++ types/utility.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Sums up two numbers.
+ *
+ * @param {number} a The first number to add.
+ * @param {number} b The second number to add.
+ * @returns {number} The resulting sum of the two numbers.
+ */
+export declare function add(a: number, b: number): number
