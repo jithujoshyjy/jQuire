c35d11625b61618ff499cf08693e537eb36b0a2b
jithujoshyjy <jithujoshyjy@gmail.com>
2023-08-04 01:54:17 +0530

    update docs to use the latest patch version

--- docs/components/CodeBox.js
+++ docs/components/CodeBox.js
@@ -1,4 +1,4 @@
-import { natives, nodes, css, state, watch } from "https://cdn.jsdelivr.net/npm/jquire@1.3.19/src/jquire.min.js"
+import { natives, nodes, css, state, watch } from "https://cdn.jsdelivr.net/npm/jquire@1.3.20/src/jquire.min.js"
 const { pre, code, link } = natives
 const { attr, fragment } = nodes
 
